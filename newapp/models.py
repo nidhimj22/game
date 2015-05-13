5f0a8ccaee702cfe57433deee49f95abf900ee49
Nidhi <nidhimj22@gmail.com>
2015-05-13 20:35:30 +0530

    changes

--- newapp/models.py
+++ newapp/models.py
@@ -11,20 +11,18 @@ class Player(models.Model):
 	majorstudy = models.CharField(max_length=100)
 	occupation = models.CharField(max_length=100)
 
-"""
 
 class Game(models.Model):
-	gametype=models.IntegerField()
-	player=models.ForeignKey(Player)
-	humanmoves=[]
-	playermoves=[]
+	player=models.ForeignKey(Player,null=True,blank=True)
+        gametype=models.IntegerField()
+	hackermoves=models.CharField(max_length=500)
+	analystmoves=models.CharField(max_length=500)
 	winner=models.CharField(max_length=60)
 	hackerscore=  models.IntegerField()
 	analystscore=models.IntegerField() #scores in the end
 	human=models.CharField(max_length=60)  #profile set
 
 
-"""
 class Feedback(models.Model):
 	player=models.ForeignKey(Player,null=True,blank=True)	
 	ownstrategy=models.CharField(max_length=100)
