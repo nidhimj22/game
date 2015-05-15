c7c050be926a283608139398274da82301539386
Nidhi <nidhimj22@gmail.com>
2015-05-15 21:49:54 +0530

    deploy

--- mysite/research.py
+++ mysite/research.py
@@ -1,5 +1,5 @@
 # equla payoffs 
-trials1 = 50
+trials1 = 10
 attack_mat1 = [[-10, 5], [0, 0]]
 defence_mat1 = [[5, -5], [-5, 0]]
 p1 = 34
@@ -8,7 +8,7 @@ multiplier1 = 75
 base1 = 500
 
 # rewarding hacker 
-trials2 = 50
+trials2 = 10
 attack_mat2 = [[-10, 50], [0, 0]]
 defence_mat2 = [[5, -5], [0, 0]]
 p2 = 34
@@ -17,7 +17,7 @@ multiplier2 = 300
 base2 = 2500
 
 #rewarding analyst
-trials3 = 50
+trials3 = 10
 attack_mat3 = [[-10, 5], [0, 0]]
 defence_mat3 = [[50, -5], [-5, 0]]
 p3 = 9
